00e1aa25c68b7e9f9552293b40ede382c05e71af
ygherri <mina.gh2060@gmail.com>
2026-05-17 13:51:07 +0200

    Adjust Angular style budget

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
